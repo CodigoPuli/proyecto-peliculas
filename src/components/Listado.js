053ac7711fec3303b924057d65465ec833868af3
Javier Pulido <jpdgranada@gmail.com>
2024-05-12 20:23:48 +0200

    add functionality to the edit and update button #1

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { Editar } from './Editar';
 
 export const Listado = ({listadoState, setListadoState}) => {
 
     // const [listadoState, setListadoState] = useState([]);
+
+    const [editar, setEditar] = useState(0)
 
     useEffect(() => {
 
@@ -44,8 +47,19 @@ export const Listado = ({listadoState, setListadoState}) => {
                         <h3 className="title">{peli.titulo}</h3>
                         <p className="description">{peli.descripcion}</p>
 
-                        <button className="edit">Editar</button>
+                        <button className="edit" onClick={ () => setEditar(peli.id)}>Editar</button>
                         <button className="delete" onClick={ () => borrarPeli(peli.id)}>Borrar</button>
+                         {/* aparece formulario de editar */}
+                        {editar === peli.id && (
+                            
+                            <Editar peli={peli}
+                                    conseguirPeliculas={conseguirPeliculas}
+                                    setEditar={setEditar}
+                                    setListadoState={setListadoState}
+                            
+                            />
+                        )}
+
                     </article>
                 )
             })
